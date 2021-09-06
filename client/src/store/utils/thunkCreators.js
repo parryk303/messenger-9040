59ec0a718c55244b10bc5f03bd97cb0ed2f1633d
Kyle Parry <kyle.parry.303@gmail.com>
2021-09-06 11:28:53 -0600

    Fix new message not appearing and message order bugs

--- client/src/store/utils/thunkCreators.js
+++ client/src/store/utils/thunkCreators.js
@@ -72,6 +72,9 @@ export const logout = (id) => async (dispatch) => {
 export const fetchConversations = () => async (dispatch) => {
   try {
     const { data } = await axios.get("/api/conversations");
+    data.forEach((conversation => {
+      conversation.messages.reverse();
+    }))
     dispatch(gotConversations(data));
   } catch (error) {
     console.error(error);
@@ -89,13 +92,14 @@ const sendMessage = (data, body) => {
     recipientId: body.recipientId,
     sender: data.sender,
   });
+  console.log(data.message);
 };
 
 // message format to send: {recipientId, text, conversationId}
 // conversationId will be set to null if its a brand new conversation
-export const postMessage = (body) => (dispatch) => {
+export const postMessage = (body) => async (dispatch) => {
   try {
-    const data = saveMessage(body);
+    const data = await saveMessage(body);
 
     if (!body.conversationId) {
       dispatch(addConversation(body.recipientId, data.message));
@@ -103,7 +107,7 @@ export const postMessage = (body) => (dispatch) => {
       dispatch(setNewMessage(data.message));
     }
 
-    sendMessage(data, body);
+    await sendMessage(data, body);
   } catch (error) {
     console.error(error);
   }
